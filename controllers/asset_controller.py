e28bfa8a89ebccd9b99b8e22591c4063c5039ce7
7yadjiman7 <yadjimancoulibaly042@gmail.com>
2025-07-06 22:53:23 +0000

    Remove redundant headers variable

--- controllers/asset_controller.py
+++ controllers/asset_controller.py
@@ -2550,9 +2550,3 @@ class PatrimoineAssetController(http.Controller):
             return {"status": "error", "message": str(e)}
 
 
-headers = {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
-    "Access-Control-Allow-Headers": "Origin, Content-Type, X-Openerp-Session-Id",
-}

bd9459d35c8daefc61b5cf0833bdf9b83dad5e9f
7yadjiman7 <yadjimancoulibaly042@gmail.com>
2025-07-15 11:04:32 +0000

    chore: use configured API base URL

--- patrimoine-mtnd/src/services/apiConfig.js
+++ patrimoine-mtnd/src/services/apiConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios"
+import { API_BASE_URL } from "@/config/api"
 
 const api = axios.create({
-    baseURL: "",
+    baseURL: API_BASE_URL,
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
